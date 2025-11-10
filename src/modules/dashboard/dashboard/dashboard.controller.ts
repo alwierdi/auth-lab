42bed166499ab37cfed2f391b051078fb0501377
Wikzyy <alwi.erdi4@gmail.com>
2025-11-10 22:00:53 +0700

    refactor structure jwt auth guard

--- src/modules/dashboard/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard/dashboard.controller.ts
@@ -2,7 +2,7 @@ import { RolesGuard } from '@modules/roles/guards/roles/roles.guard';
 import { Roles } from '@modules/roles/role.decorator';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard/jwt-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard/jwt-auth.guard';
 
 // @UseGuards(AuthGuard('jwt'), RolesGuard)
 @UseGuards(JwtAuthGuard, RolesGuard)
