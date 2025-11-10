4ccd86ab9a9daa838121ca5e8e32ba2b502f56ef
Wikzyy <alwi.erdi4@gmail.com>
2025-11-10 21:57:39 +0700

    update/user-controller: set jwt guard

--- src/modules/users/users/users.controller.ts
+++ src/modules/users/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UsersController {
   constructor(private userService: UsersService) {}
